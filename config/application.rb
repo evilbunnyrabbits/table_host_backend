e8c953d16bceb90926d77b52480e191b95de1b48
BenGarlock <ben.garlock@gmail.com>
2020-10-06 10:41:36 -0400

    test

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,6 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
 
 
 module BackEnd
