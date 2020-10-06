4ae72b06988f70792d72590dc22e4becb083a4eb
BenGarlock <ben.garlock@gmail.com>
2020-10-06 10:34:39 -0400

    initial commit

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.assets.initialize_on_precompile = false
+
 
 module BackEnd
   class Application < Rails::Application
